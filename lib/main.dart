e526383006f261ae22ce8a7a235b7cb295c8fdcd
fasterbarry <114644983+fasterbarry@users.noreply.github.com>
2023-10-03 23:22:33 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Practica 09',
+      title: 'Practica 10',
       home: ListPage(),
     );
   }
