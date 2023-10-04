90813fc5095fad7907f76bfdba78b499308a8c8c
fasterbarry <114644983+fasterbarry@users.noreply.github.com>
2023-10-03 23:25:16 -0700

    Update listview_page.dart

--- lib/src/listview_page.dart
+++ lib/src/listview_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ListPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    List<String> types = [
+  List<String> types = [
       'Normal',
       'Lucha',
       'Volador',
@@ -24,9 +22,11 @@ class ListPage extends StatelessWidget {
       'Hada',
       'Unknown',
     ];
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Practica 09 - Pokédex'),
+        title: Text('Practica 10 - Pokédex'),
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
@@ -39,7 +39,7 @@ class ListPage extends StatelessWidget {
                         top: 5, left: 10, right: 10, bottom: 0.0),
                     title: Text(types[index]),
                     leading: const CircleAvatar(
-                      radius: 22.0,
+                      radius: 16.0,
                       backgroundImage: NetworkImage(
                           'https://th.bing.com/th/id/R.09da926c2b94d95008a9e3b2f60bfdd3?rik=KWyWFVOUW0NTDQ&pid=ImgRaw&r=0'),
                       backgroundColor: Colors.white,
